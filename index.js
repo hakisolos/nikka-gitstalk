c88957b39fd887942b9823ef18f8b2480c6c5ae0
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-29 19:13:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,55 @@ app.get('/api/githubstalk', async (req, res) => {
     }
 });
 
+
+
+async function npmstalk(packageName) {
+  let stalk = await axios.get("https://registry.npmjs.org/" + packageName);
+  let versions = stalk.data.versions;
+  let allver = Object.keys(versions);
+  let verLatest = allver[allver.length - 1];
+  let verPublish = allver[0];
+  let packageLatest = versions[verLatest];
+  
+  return {
+    name: packageName,
+    versionLatest: verLatest,
+    versionPublish: verPublish,
+    versionUpdate: allver.length,
+    latestDependencies: Object.keys(packageLatest.dependencies || {}).length,
+    publishDependencies: Object.keys(versions[verPublish].dependencies || {}).length,
+    publishTime: stalk.data.time.created,
+    latestPublishTime: stalk.data.time[verLatest],
+  };
+}
+
+// API endpoint to get package details with a query parameter
+app.get('/api/npmstalk', async (req, res) => {
+  const { q: packageName } = req.query; // Extract 'q' from query parameters
+
+  if (!packageName) {
+    return res.status(400).json({
+      success: false,
+      message: "Query parameter 'q' is required",
+    });
+  }
+
+  try {
+    const packageDetails = await npmstalk(packageName);
+    res.json({
+      success: true,
+      data: packageDetails,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to fetch package details',
+      error: error.message,
+    });
+  }
+});
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
